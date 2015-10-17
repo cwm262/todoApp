715430e936c6ae8091f055fd5b92e244e6b66ceb
Cody McCarson <azathoth.hp@gmail.com>
2015-10-16 23:16:08 -0500

    Added checkbox functionality. Redid items left todo logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,22 +8,25 @@
  * Controller of the todoAppApp
  */
 angular.module('todoAppApp')
-  .controller('MainCtrl', function ($scope, localStorageService) {
+  .controller('MainCtrl', function ($scope, $filter, localStorageService) {
     var todosInStore = localStorageService.get('todos');
 
     $scope.todos = todosInStore || [];
 
     $scope.$watch('todos', function () {
+      $scope.remainingCount = $filter('filter')($scope.todos, { isDone: false}).length;
+      $scope.completedCount = $scope.todos.length - $scope.remainingCount;
       localStorageService.set('todos', $scope.todos);
     }, true);
 
     $scope.addTodo = function(){
-      $scope.todos.push($scope.todo);
-      $scope.todo = '';
+      $scope.todos.push({taskName : $scope.todo , isDone : false });
+      $scope.todo = '';//Reset the text field.
     };
 
     $scope.removeTodo = function (index) {
       $scope.todos.splice(index, 1);
     };
-    
+
+
   });
